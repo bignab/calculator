5760eb1d1f07a1d8156941817cdd0411de508d70
bignab <nabilrafatisb@gmail.com>
2021-10-12 19:13:04 -0400

    calculator visual design complete, preliminary button wiring implemented

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,10 @@ function multiply(x, y) {
     return x * y;
 }
 
+function modulo(x, y) {
+    return x * y;
+}
+
 function operate(firstNumber, secondNumber, operation) {
     switch (operation) {
         case 'add':
@@ -34,6 +38,106 @@ function operate(firstNumber, secondNumber, operation) {
         case 'multiply':
             multiply(firstNumber, secondNumber);
             break;
-
+        case 'modulo':
+            modulo(firstNumber, secondNumber);
+            break;
     }
 }
+
+const calcScreenTextOne = document.querySelector('#calcScreenTextOne');
+const calcScreenTextTwo = document.querySelector('#calcScreenTextTwo');
+
+const btnModulo = document.querySelector('#btnModulo');
+const btnAdd = document.querySelector('#btnAdd');
+const btnMinus = document.querySelector('#btnMinus');
+const btnDivide = document.querySelector('#btnDivide');
+const btnMultiply = document.querySelector('#btnMultiply');
+// const btnEquals = document.querySelector('#btnEquals');
+
+const btnAC = document.querySelector('#btnAC');
+const btnC = document.querySelector('#btnC');
+const btnDecimal = document.querySelector('#btnDecimal');
+const btnPlusMinus = document.querySelector('#btnPlusMinus');
+
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const btn4 = document.querySelector('#btn4');
+const btn5 = document.querySelector('#btn5');
+const btn6 = document.querySelector('#btn6');
+const btn7 = document.querySelector('#btn7');
+const btn8 = document.querySelector('#btn8');
+const btn9 = document.querySelector('#btn9');
+const btn0 = document.querySelector('#btn0');
+
+btn1.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '1';
+  });
+
+btn2.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '2';
+});
+
+btn3.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '3';
+});
+
+btn4.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '4';
+});
+
+btn5.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '5';
+});
+
+btn6.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '6';
+});
+
+btn7.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '7';
+});
+
+btn8.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '8';
+});
+
+btn9.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '9';
+});
+
+btn0.addEventListener('click', () => {
+    calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '0';
+  });
+
+btnModulo.addEventListener('click', () => {
+    calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " %";
+    calcScreenTextTwo.textContent = '';
+});
+
+btnAdd.addEventListener('click', () => {
+    calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " +";
+    calcScreenTextTwo.textContent = '';
+});
+
+btnMinus.addEventListener('click', () => {
+    calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " −";
+    calcScreenTextTwo.textContent = '';
+});
+
+btnDivide.addEventListener('click', () => {
+    calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " ÷";
+    calcScreenTextTwo.textContent = '';
+});
+
+btnMultiply.addEventListener('click', () => {
+    calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " ×";
+    calcScreenTextTwo.textContent = '';
+});
+
+btnEquals.addEventListener('click', () => {
+    if (calcScreenTextOne.textContent != '') {
+        calcScreenTextOne.textContent = calcScreenTextOne.textContent + " " + calcScreenTextTwo.textContent + " =";
+        calcScreenTextTwo.textContent = '';
+    }
+});
