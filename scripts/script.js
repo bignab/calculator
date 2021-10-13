1c0df3432f30a2eb15c7a554f3179283fbe6db47
bignab <nabilrafatisb@gmail.com>
2021-10-13 18:20:04 -0400

    all functions added, logic wiring completed, added title, calculator functioining

--- scripts/script.js
+++ scripts/script.js
@@ -21,27 +21,27 @@ function multiply(x, y) {
 }
 
 function modulo(x, y) {
-    return x * y;
+    return x % y;
 }
 
-function operate(firstNumber, secondNumber, operation) {
+function operate(firstNumber, operation, secondNumber) {
     switch (operation) {
-        case 'add':
-            add(firstNumber, secondNumber);
-            break;
-        case 'subtract':
-            subtract(firstNumber, secondNumber);
-            break;
-        case 'divide':
-            divide(firstNumber, secondNumber);
-            break;
-        case 'multiply':
-            multiply(firstNumber, secondNumber);
-            break;
-        case 'modulo':
-            modulo(firstNumber, secondNumber);
-            break;
+        case '+':
+            return add(+firstNumber, +secondNumber);
+        case '−':
+            return subtract(+firstNumber, +secondNumber);
+        case '÷':
+            return divide(+firstNumber, +secondNumber);
+        case '×':
+            return multiply(+firstNumber, +secondNumber);
+        case '%':
+            return modulo(+firstNumber, +secondNumber);
     }
+}
+
+function parseInput(equationText) {
+    let arrParsed = equationText.split(' ');
+    return arrParsed;
 }
 
 const calcScreenTextOne = document.querySelector('#calcScreenTextOne');
@@ -52,7 +52,7 @@ const btnAdd = document.querySelector('#btnAdd');
 const btnMinus = document.querySelector('#btnMinus');
 const btnDivide = document.querySelector('#btnDivide');
 const btnMultiply = document.querySelector('#btnMultiply');
-// const btnEquals = document.querySelector('#btnEquals');
+const btnEquals = document.querySelector('#btnEquals');
 
 const btnAC = document.querySelector('#btnAC');
 const btnC = document.querySelector('#btnC');
@@ -71,73 +71,161 @@ const btn9 = document.querySelector('#btn9');
 const btn0 = document.querySelector('#btn0');
 
 btn1.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '1';
   });
 
 btn2.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '2';
 });
 
 btn3.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '3';
 });
 
 btn4.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '4';
 });
 
 btn5.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '5';
 });
 
 btn6.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '6';
 });
 
 btn7.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '7';
 });
 
 btn8.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '8';
 });
 
 btn9.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '9';
 });
 
 btn0.addEventListener('click', () => {
+    noInitialZero();
     calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + '0';
   });
 
 btnModulo.addEventListener('click', () => {
+    if (checkIfRepeatOperations()) {
+        let arrParsed = parseInput(calcScreenTextOne.textContent);
+        calcScreenTextTwo.textContent = operate(arrParsed[0], arrParsed[1], calcScreenTextTwo.textContent);
+    }
     calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " %";
-    calcScreenTextTwo.textContent = '';
+    calcScreenTextTwo.textContent = '0';
 });
 
 btnAdd.addEventListener('click', () => {
+    if (checkIfRepeatOperations()) {
+        let arrParsed = parseInput(calcScreenTextOne.textContent);
+        calcScreenTextTwo.textContent = operate(arrParsed[0], arrParsed[1], calcScreenTextTwo.textContent);
+    }
     calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " +";
-    calcScreenTextTwo.textContent = '';
+    calcScreenTextTwo.textContent = '0';
 });
 
 btnMinus.addEventListener('click', () => {
+    if (checkIfRepeatOperations()) {
+        let arrParsed = parseInput(calcScreenTextOne.textContent);
+        calcScreenTextTwo.textContent = operate(arrParsed[0], arrParsed[1], calcScreenTextTwo.textContent);
+    }
     calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " −";
-    calcScreenTextTwo.textContent = '';
+    calcScreenTextTwo.textContent = '0';
 });
 
 btnDivide.addEventListener('click', () => {
+    if (checkIfRepeatOperations()) {
+        let arrParsed = parseInput(calcScreenTextOne.textContent);
+        calcScreenTextTwo.textContent = operate(arrParsed[0], arrParsed[1], calcScreenTextTwo.textContent);
+    }
     calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " ÷";
-    calcScreenTextTwo.textContent = '';
+    calcScreenTextTwo.textContent = '0';
 });
 
 btnMultiply.addEventListener('click', () => {
+    if (checkIfRepeatOperations()) {
+        let arrParsed = parseInput(calcScreenTextOne.textContent);
+        calcScreenTextTwo.textContent = operate(arrParsed[0], arrParsed[1], calcScreenTextTwo.textContent);
+    }
     calcScreenTextOne.textContent = calcScreenTextTwo.textContent + " ×";
-    calcScreenTextTwo.textContent = '';
+    calcScreenTextTwo.textContent = '0';
+});
+
+btnPlusMinus.addEventListener('click', () => {
+    if (calcScreenTextTwo.textContent.substring(0,1) == "-") {
+        calcScreenTextTwo.textContent = calcScreenTextTwo.textContent.slice(1);
+    }
+    else {
+        calcScreenTextTwo.textContent = '-' + calcScreenTextTwo.textContent;
+    }
 });
 
 btnEquals.addEventListener('click', () => {
     if (calcScreenTextOne.textContent != '') {
         calcScreenTextOne.textContent = calcScreenTextOne.textContent + " " + calcScreenTextTwo.textContent + " =";
-        calcScreenTextTwo.textContent = '';
+        let arrParsed = parseInput(calcScreenTextOne.textContent);
+        calcScreenTextTwo.textContent = "" + operate(arrParsed[0], arrParsed[1], arrParsed[2]);
     }
 });
+
+btnAC.addEventListener('click', () => {
+    calcScreenTextOne.textContent = '';
+    calcScreenTextTwo.textContent = '0';
+});
+
+btnC.addEventListener('click', () => {
+        calcScreenTextTwo.textContent = calcScreenTextTwo.textContent.substring(0, calcScreenTextTwo.textContent.length-1);
+        if (calcScreenTextTwo.textContent == ("") || calcScreenTextTwo.textContent == ("-")) {
+            calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + "0";
+        }
+});
+
+btnDecimal.addEventListener('click', () => {
+    if (checkIfDecimal() == false) {
+        calcScreenTextTwo.textContent = calcScreenTextTwo.textContent + ".";
+    }
+});
+
+function noInitialZero() {
+    if (calcScreenTextTwo.textContent.substring(0,1) == "-") {
+        if (calcScreenTextTwo.textContent.substring(1,2) == "0") {
+            calcScreenTextTwo.textContent = "-";
+        }
+    }
+    else if (calcScreenTextTwo.textContent.substring(0,1) == "0") {
+        calcScreenTextTwo.textContent = "";
+    }
+}
+
+function checkIfRepeatOperations() {
+    if (parseInput(calcScreenTextOne.textContent).length == 2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function checkIfDecimal() {
+    let reDot = /[.]/g;
+    if (calcScreenTextTwo.textContent.search(reDot) > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
